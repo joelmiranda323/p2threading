cce9dfa198541d94c9fbf3de21495ab224707af7
Ali Osmani <77931122+aosmani38@users.noreply.github.com>
2022-10-26 10:34:31 -0700

    Update sem.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -14,6 +14,7 @@ struct semaphore {
 sem_t sem_create(size_t count)
 {
 	/* TODO Phase 3 */
+    //create a semaphore
     struct semaphore *sem = malloc(sizeof(struct semaphore));
     sem->counter = count;
     sem->queue = queue_create();
@@ -24,15 +25,49 @@ sem_t sem_create(size_t count)
 int sem_destroy(sem_t sem)
 {
 	/* TODO Phase 3 */
+    if(sem == NULL || queue_length(sem->queue) > 0) {
+        return -1;
+    } else {
+        free(sem);
+        return 0;
+    }
 }
 
 int sem_down(sem_t sem)
 {
 	/* TODO Phase 3 */
+    //check if there is any resources available
+    if(sem == NULL) {
+        return -1;
+    }
+    
+    if(sem->counter <= 0) {
+        // if there is no resources available, get the current thread and add it to the queue and block it
+        struct uthread_tcb *current = uthread_current();
+        queue_enqueue(sem->queue,current);
+        uthread_block();
+    } else {
+        //else decrement that the resource is being used
+        sem->counter--;
+        return 0;
+    }
+
 }
 
 int sem_up(sem_t sem)
 {
 	/* TODO Phase 3 */
+
+    //check if there is a thread in the queue waiting
+    if(queue_length(sem->queue) > 0) {
+        // get it from the queue and unblock it
+        queue_dequeue(sem->queue, (void**)uthread_current());
+        uthread_unblock(uthread_current());
+    }
+
+    //make the resource available again
+    sem->counter++;
+    return 0;
+
 }
 
