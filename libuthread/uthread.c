1e60fcb7433d89ef93666b85b4f691f71bb2f5a6
joelmiranda323 <62614737+joelmiranda323@users.noreply.github.com>
2022-10-26 21:43:46 -0700

    Add files via upload

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -13,10 +13,10 @@
 #define READY	0
 #define	RUNNING	1
 #define BLOCKED	2
-#define ZOMBIE	3
+#define EXIT	3
 
 // Race condtions
-// We need to use a lock (preempt disable/enable functions) for these
+// We need to use a lock for these
 queue_t qThread;
 struct uthread_tcb *currentThread;
 
@@ -37,98 +37,114 @@ void uthread_yield(void)
 {
 	/* TODO Phase 2 */
 
-	preempt_disable();
-	// Get currrent running thread
-	struct uthread_tcb *currThread = uthread_current();
+	// Get currrent running thread which change to a READY state
+	struct uthread_tcb *prevThread = uthread_current();
 
 	// yield: from RUNNING to READY
-	if (currThread->state == RUNNING) {
+	if (prevThread->state == RUNNING) {
 		// Currrent running thread is now yielding
-		currThread->state = READY;
+		prevThread->state = READY;
 
 		// Add yeilding thread to the queue
-		queue_enqueue(qThread, (void *) currThread);
+		queue_enqueue(qThread, (void *) prevThread);
 	}
 	else {
 		// print the state of the current thread
-		printf("In uthread_yield(): currThread\n");
-		printf("State of current thread: %d\n", currThread->state);
+		perror("uthread_yield(): prevThread");
+		printf("State of current thread: %d\n", prevThread->state);
 	}
 
-	// Get the oldest thread from the queue
-	struct uthread_tcb *oldThread;
-	queue_dequeue(qThread, (void **) &oldThread);
+	// Get the oldest thread from queue which will change to a RUNNING state
+	struct uthread_tcb *nextThread;
+	queue_dequeue(qThread, (void **) &nextThread);
 
 	// Elect oldest thread to run: READY to RUNNING
-	if (oldThread->state == READY) {
+	if (nextThread->state == READY) {
 		// Oldest thread is now the current running thread
-		oldThread->state = RUNNING;
-		currentThread = oldThread;
+		nextThread->state = RUNNING;
+		currentThread = nextThread;
 	}
 	else {
 		// print the state of the oldest thread
-		printf("In uthread_yield(): oldThread\n");
-		printf("State of oldest thread: %d\n", oldThread->state);
+		perror("uthread_yield(): nextThread");
+		printf("State of oldest thread: %d\n", nextThread->state);
 	}
 
 	// Save yielding threads context and activate current threads context
-	uthread_ctx_switch(currThread->context, oldThread->context);
-	preempt_enable();
+	uthread_ctx_switch(prevThread->context, nextThread->context);
 }
 
 void uthread_exit(void)
 {
 	/* TODO Phase 2 */
-	preempt_disable();
+
 	// Get currrent running thread
-	struct uthread_tcb *currThread = uthread_current();
+	struct uthread_tcb *prevThread = uthread_current();
 
 	// Exit current running thread
-	currThread->state = ZOMBIE;
-	free(currThread->context);
-	free(currThread->stack);
+	prevThread->state = EXIT;
+	free(prevThread->context);
+	free(prevThread->stack);
 
-	// Get the oldest thread from the queue
-	struct uthread_tcb *oldThread;
-	queue_dequeue(qThread, (void **) &oldThread);
+	// Get the oldest thread from queue which will change to a RUNNING state
+	struct uthread_tcb *nextThread;
+	queue_dequeue(qThread, (void **) &nextThread);
 
 	// Elect oldest thread to run: READY to RUNNING
-	if (oldThread->state == READY) {
+	if (nextThread->state == READY) {
 		// Oldest thread is now the current running thread
-		oldThread->state = RUNNING;
-		currentThread = oldThread;
+		nextThread->state = RUNNING;
+		currentThread = nextThread;
 	}
 	else {
 		// print the state of the oldest thread
-		printf("In uthread_exit(): oldThread\n");
-		printf("State of oldest thread: %d\n", oldThread->state);
+		perror("uthread_exit()");
+		printf("State of oldest thread: %d\n", nextThread->state);
 	}
 
 	// Save yielding threads context and activate current threads context
-	uthread_ctx_switch(currThread->context, oldThread->context);
-	preempt_enable();
+	uthread_ctx_switch(prevThread->context, nextThread->context);
 }
 
 int uthread_create(uthread_func_t func, void *arg)
 {
 	/* TODO Phase 2 */
+
+	// Create a new thread
 	int ctxInitStatus;
 	struct uthread_tcb *newThread = (struct uthread_tcb *) malloc(sizeof(struct uthread_tcb));
+
+	if (newThread == NULL) {
+		perror("uthread_create()");
+		printf("could NOT allocate memory for newThread\n");
+		return -1;
+	}
 	
 	newThread->state = READY;
 	newThread->context = (struct uthread_ctx_t *) malloc(sizeof(uthread_ctx_t));
 	newThread->stack = uthread_ctx_alloc_stack();
 
-	ctxInitStatus = uthread_ctx_init(newThread->context, newThread->stack, func, arg);
-
-	if(ctxInitStatus == -1) {
-		perror("ctxInitStatus == -1 in uthread_create()");
+	if (newThread->context == NULL) {
+		perror("uthread_create()");
+		printf("could NOT allocate memory for newThread->context\n");
+		return -1;
+	}
+	else if (newThread->stack == NULL) {
+		perror("uthread_create()");
+		printf("could NOT allocate memory for newThread->stack\n");
 		return -1;
 	}
 
-	preempt_disable();
+	// Initiate the new threads context
+	ctxInitStatus = uthread_ctx_init(newThread->context, newThread->stack, func, arg);
+
+	if(ctxInitStatus == -1) {
+		perror("ctxInitStatus in uthread_create()");
+		return -1;
+	}
+
+	// Add the new thread to the ready queue
 	queue_enqueue(qThread, (void *) newThread);
-	preempt_enable();
 
 	return 0;
 }
@@ -137,16 +153,34 @@ int uthread_run(bool preempt, uthread_func_t func, void *arg)
 {
 	/* TODO Phase 2 */
 
+	// Create an main/idle thread
+	struct uthread_tcb *idleThread = (struct uthread_tcb *) malloc(sizeof(struct uthread_tcb));
+
+	if (idleThread == NULL) {
+		perror("uthread_run()");
+		printf("could NOT allocate memory for idleThread\n");
+		return -1;
+	}
+
+	idleThread->state = RUNNING;
+	
 	// Start preemption
 	preempt_start(preempt);
 
+	// The main/idle thread is the current running thread
+	currentThread = idleThread;
+
 	// Initialize and allocate memory to global queue
-	preempt_disable();
 	qThread = queue_create();
-	preempt_enable();
 
 	// Create first thread
 	uthread_create(func, arg);
+
+	while (queue_length(qThread) != 0) {
+		uthread_yield();
+	}
+
+	return 0;
 }
 
 void uthread_block(void)
