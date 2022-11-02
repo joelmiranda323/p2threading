99a7c2643d8128786d5d3327e20794cb15659962
Ali Osmani <77931122+aosmani38@users.noreply.github.com>
2022-11-02 16:33:38 -0700

    Add files via upload

---
+++ libuthread/preempt_test.c
@@ -0,0 +1,34 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sem.h"
+#include "uthread.h"
+
+static void thread3(void *arg)
+{
+	(void)arg;
+	while(1){printf("thread3\n");}
+}
+
+static void thread2(void *arg)
+{
+	(void)arg;
+	while(1){printf("thread2\n");}
+	
+}
+
+static void thread1(void *arg)
+{
+	(void)arg;
+
+	uthread_create(thread2, NULL);
+	uthread_create(thread3, NULL);
+	while(1){printf("thread1\n");}
+}
+
+int main(void)
+{
+	uthread_run(false, thread1, NULL);
+	return 0;
+}
