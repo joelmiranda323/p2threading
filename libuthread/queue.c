0c4e1743f05ba7498951981aa9a9641613c7babd
Ali Osmani <77931122+aosmani38@users.noreply.github.com>
2022-11-03 10:46:22 -0700

    Update queue.c

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -76,21 +76,29 @@ int queue_enqueue(queue_t queue, void *data)
 int queue_dequeue(queue_t queue, void **data)
 {
     /* TODO Phase 1 */
+    
+    // check if the queue or data is NULL
     if(queue == NULL || data == NULL) {
         return -1;
     }
-
+    
+    // make the passed pointer to point to the memory address of the data pointer of the front node
     *data = queue->front->data;
-
+    
+    // create a node named old that will point to the front. we save it here so we can destro it later
     struct node *old = 0;
     old = queue->front;
-
+    
+    // removing the front by make the next node the front. basically, remove the first node from the queue and decrease the size by one
     queue->front = queue->front->next;
     queue->size = queue->size - 1;
+    
+    // if there was only one node in the queue the front will be pointing to NULL after we remove it, so, we will also make the rear points to NULL
     if(queue->front == NULL) {
         queue->rear = NULL;
     }
-
+    
+    //destroy the node that was removed from the queue
     free(old);
     return 0;
 }
@@ -98,16 +106,23 @@ int queue_dequeue(queue_t queue, void **data)
 int queue_delete(queue_t queue, void *data)
 {
     /* TODO Phase 1 */
+    
+    // if the passed data is on the front of the queue, just remove it, decrement the size, and return 0. 
     if(queue->front->data == data) {
         queue->front = queue->front->next;
         queue->size = queue->size -1;
         return 0;
     }
+    
+    // the idea is to create two nodes current and previous to find the passed data
     struct node *current = queue->front->next;
     struct node *previous = queue->front;
 
-
+    
+    // go thought the each node of the queue using current and previous, check if the current points to the passed data? 
     for(int i=0; i < (queue->size - 1);i++){
+        
+        // if the current pointer points to passed data, remove and destry it and return 0;
         if(current->data == data) {
             previous->next = current->next;
             free(current);
@@ -115,13 +130,15 @@ int queue_delete(queue_t queue, void *data)
             queue->size = queue->size - 1;
             return 0;
         }
-
+        
+        // looking for the pointer that points to the passed data
         if(current->next != 0) {
             current = current->next;
         }
         previous = previous->next;
     }
-
+    
+    // if not found, return -1.
     return -1;
 }
 
@@ -131,9 +148,14 @@ int queue_iterate(queue_t queue, queue_func_t func)
     if(queue == NULL || func == NULL) {
         return -1;
     }
+    
+    // store size of the queue
     int actual = queue->size;
-
+    
+    // make a temp pointer to iterate through the queue
     struct node *temp = queue->front;
+    
+    //iterate through
     while(temp->next != NULL) {
         func(queue, temp->data);
         if(queue->size != actual ) {
@@ -150,5 +172,7 @@ int queue_length(queue_t queue)
 {
     /* TODO Phase 1 */
     if(queue == NULL) {return -1;}
+    
+    // return the size if the queue is not null
     return queue->size;
 }
